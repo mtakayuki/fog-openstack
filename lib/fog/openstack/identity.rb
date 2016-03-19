17a6fefdef267d99d37b0a2326f0b6c569cb303e
Takayuki Maeda <mtakayuki1209@gmail.com>
2016-03-19 18:23:24 +0900

    Fix logic to detect identity version

--- lib/fog/openstack/identity.rb
+++ lib/fog/openstack/identity.rb
@@ -3,25 +3,37 @@
 module Fog
   module Identity
     class OpenStack < Fog::Service
+      requires :openstack_auth_url
 
       # Fog::Identity::OpenStack.new() will return a Fog::Identity::OpenStack::V2 or a Fog::Identity::OpenStack::V3,
       #  depending on whether the auth URL is for an OpenStack Identity V2 or V3 API endpoint
       def self.new(args = {})
-        @openstack_auth_uri = URI.parse(args[:openstack_auth_url]) if args[:openstack_auth_url]
         if self.inspect == 'Fog::Identity::OpenStack'
-          service = (is_v3? args) ? Fog::Identity::OpenStack::V3.new(args) : Fog::Identity::OpenStack::V2.new(args)
+          identity = super(args.select { |key, _value| key == :openstack_auth_url })
+          service = identity.v3? ? Fog::Identity::OpenStack::V3.new(args) : Fog::Identity::OpenStack::V2.new(args)
         else
           service = Fog::Service.new(args)
         end
         service
       end
 
-      private
+      module Version
+        def initialize(options = {})
+          @openstack_auth_uri = URI.parse(options[:openstack_auth_url])
+        end
 
-      def self.is_v3?(args)
-        @openstack_auth_uri && @openstack_auth_uri.path =~ /\/v3/
+        def v3?
+          @openstack_auth_uri && @openstack_auth_uri.path =~ /\/v3/
+        end
       end
 
+      class Mock
+        include Version
+      end
+
+      class Real
+        include Version
+      end
     end
   end
 end
